a6c5f07c73028577ae38ce72add2c8d2c4894dad
Martin <martin.g.cao@gmail.com>
2023-02-26 21:07:33 +1100

    Load Google fonts in header.blade

--- web/app/themes/sage/resources/views/sections/header.blade.php
+++ web/app/themes/sage/resources/views/sections/header.blade.php
@@ -8,4 +8,9 @@
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     </nav>
   @endif
+
+  {{-- Google Fonts --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 </header>
